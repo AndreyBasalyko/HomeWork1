47e03a8ef77404e5b86440f7ff57dba57d906da7
Андрей Басалыко <basalykoa.s@gmail.com>
2022-07-04 15:54:11 +0300

    Выполнил третью задачу.

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число...");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 2 == 0)
+{
+   Console.WriteLine("Четное"); 
+}
+else
+{
+   Console.WriteLine("Нечетное"); 
+}
