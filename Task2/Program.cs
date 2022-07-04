87fa9d33b104d4f6a7bf9f8af3d237e5ab11ef06
Андрей Басалыко <basalykoa.s@gmail.com>
2022-07-04 15:46:45 +0300

    Выполнил вторую задачу.

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число...");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число...");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число...");
+int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+int MaxNumber = FirstNumber;
+if(MaxNumber < SecondNumber)
+{
+    MaxNumber = SecondNumber;
+}
+if (MaxNumber < ThirdNumber)
+{
+    MaxNumber = ThirdNumber;
+}
+Console.WriteLine("Максимальное число");
+Console.WriteLine(MaxNumber);
