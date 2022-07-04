de5d53574eff7363f72c8441030ef5356877eddf
Андрей Басалыко <basalykoa.s@gmail.com>
2022-07-04 16:38:03 +0300

    Выполнили последнюю задачу.

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число.");
+int Number = Convert.ToInt32(Console.ReadLine());
+int H = 1;
+while (Number > H)
+{
+     if (H % 2 == 0)
+ {
+    Console.Write(H);
+    Console.Write(";");
+    H++;
+ }
+ else
+ {
+    H++;
+ }
+}
+if (Number % 2 == 0)
+{
+   Console.WriteLine(Number);
+}
