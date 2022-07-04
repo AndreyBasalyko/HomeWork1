255fd4cf6acf7511df5232a40657f930ca6c3e98
Андрей Басалыко <basalykoa.s@gmail.com>
2022-07-04 14:49:14 +0300

    Создал Первую задачу.

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число...");
+int FirstNumber = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите второе число...");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+if(FirstNumber < SecondNumber)
+{
+    int maxNumber = SecondNumber;
+    int minNumber = FirstNumber;
+    Console.WriteLine("Большее число");
+    Console.WriteLine(SecondNumber);
+    Console.WriteLine("Меньшее число");
+    Console.WriteLine(FirstNumber);
+}
+else
+{
+   Console.WriteLine("Большее число...");
+   Console.WriteLine(FirstNumber);
+   Console.WriteLine("Меньшее число...");
+   Console.WriteLine(SecondNumber);
+}
+
